61a9bcbe38881a2db988cfc4284c6b4bbcfa895f
Lucas Fabio <macbookair@MacBook-Air-de-MacBook.local>
2020-10-03 13:36:24 -0300

    Ordem imports

--- backend/medicar/usuarios/views.py
+++ backend/medicar/usuarios/views.py
@@ -1,9 +1,10 @@
-from django.contrib.auth import authenticate
 from rest_framework import status
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
+
+from django.contrib.auth import authenticate
 
 from .models import Usuario
 from .serializers import UsuarioSerializer
